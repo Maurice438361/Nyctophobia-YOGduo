922ee435669db7f5c37fbd215f478b2a74593c95
bradreslam <105541244+bradreslam@users.noreply.github.com>
2025-04-08 09:20:29 +0200

    Added wall jump
    I finished fleshing out the wall jump, it just needs to be checked for bugs and configured correctly.

--- assets/Player sprite/nyctophobia_player.gd
+++ assets/Player sprite/nyctophobia_player.gd
@@ -4,8 +4,14 @@ extends CharacterBody2D
 @export var SPEED : int = 150
 @export var GRAVITY : int = 900
 @export var JUMP_FORCE : int = 300
+@export var WALL_CLING_TIME : float = 1
+@export var WALL_JUMP_FORCE : int = 200
+@export var WALL_PUSH_FORCE : int = 300
+@export var WALL_JUMP_GRACE : float = 0.5
 
-
+var WallCling = false
+var WallClingRest = false
+var WallJumped = false
 
 func _physics_process(delta):
 	
@@ -13,14 +19,14 @@ func _physics_process(delta):
 	
 	var direction = Input.get_axis("Left", "Right")
 	
-	if direction: 
+	if direction and !WallJumped: 
 		
 		velocity.x = direction * SPEED
 		
 		if is_on_floor():
 			$AnimatedSprite2D.play("Run")
 		
-	else: 
+	elif !WallJumped: 
 		
 		velocity.x = 0
 		
@@ -36,7 +42,7 @@ func _physics_process(delta):
 	
 	#Gravity
 	
-	if not is_on_floor():
+	if !is_on_floor() and !WallCling:
 		
 		velocity.y += GRAVITY * delta
 	
@@ -48,4 +54,31 @@ func _physics_process(delta):
 		
 		$AnimatedSprite2D.play("Jump")
 	
+	#WallJump
+	
+	if !is_on_floor() and is_on_wall() and !WallClingRest and velocity.y > 0:
+		var wallCollisionSide = get_wall_normal()
+		if wallCollisionSide == Vector2(1.0,0.0) and Input.is_action_pressed("Left") or wallCollisionSide == Vector2(-1.0,0.0) and Input.is_action_pressed("Right"):
+			WallCling = true
+			WallClingRest = true
+			velocity.y = 0
+			await get_tree().create_timer(WALL_CLING_TIME).timeout
+			WallCling = false
+			
+	
+	if is_on_floor() and WallClingRest:
+		WallClingRest = false
+		
+	if !is_on_wall() and WallClingRest:
+		WallCling = false
+		
+	if WallCling and Input.is_action_just_pressed("Jump"):
+		velocity.y -= WALL_JUMP_FORCE
+		velocity.x = -direction + WALL_PUSH_FORCE
+		WallJumped = true
+		WallCling = false
+		await get_tree().create_timer(WALL_JUMP_GRACE).timeout
+		WallJumped = false
+		
 	move_and_slide()
+	
