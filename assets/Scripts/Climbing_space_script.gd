927477bb8314b8622e43576593615e0e6ac5545f
bradreslam <105541244+bradreslam@users.noreply.github.com>
2025-04-14 13:53:22 +0200

    Revert "Merge branch 'Wall_climb'"
    This reverts commit 5a356128cbdd9c7e3319e418e647fcebade690b6, reversing
    changes made to abf37746b855f639f366ecce65c6f809482d3d5d.

--- assets/Scripts/Climbing_space_script.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		body.set_climbing_area_overlap(true)
-
-func _on_body_exited(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		body.set_climbing_area_overlap(false)
