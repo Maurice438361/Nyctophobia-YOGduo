e1dd96b86c600bf670204c02af5d99c0d19715a7
MauricedOuden <438361@student.fontys.nl>
2025-04-14 08:35:59 +0200

    Revert "Merge branch 'master' of https://github.com/Maurice438361/Nyctophobia-YOGduo"
    This reverts commit 853a536fb7736299c6688b7b4541d1061448bcb2, reversing
    changes made to 4a6ff8531e0542228ba4b90261b0eb6615c1907d.

--- assets/Player script/nyctophobia_player.gd
+++ assets/Player script/nyctophobia_player.gd
@@ -4,38 +4,14 @@ extends CharacterBody2D
 @export var SPEED : int = 150
 @export var GRAVITY : int = 900
 @export var JUMP_FORCE : int = 300
-@export var WALL_CLING_TIME : float = 0.2
+@export var WALL_CLING_TIME : float = 1
 @export var WALL_JUMP_FORCE : int = 200
 @export var WALL_PUSH_FORCE : int = 300
-@export var WALL_JUMP_GRACE : float = 0.2
+@export var WALL_JUMP_GRACE : float = 0.5
 
 var WallCling = false
 var WallClingRest = false
-var CantWalk = false
-var HangingFromWall = false
-var LedgeGrabRest = false
-
-@onready var wall_check_top = $TopCollisionChecker
-@onready var wall_check_bottom = $BottomCollisionChecker
-@onready var ledge_check = $LedgeCollisionChecker
-
-func is_touching_wall_full_side() -> bool:
-	var top_overlapping = wall_check_top.has_overlapping_bodies()
-	var bottom_overlapping = wall_check_bottom.has_overlapping_bodies()
-	return top_overlapping and bottom_overlapping
-	
-func is_touching_ledge_side() -> bool:
-	var wall_side_touched = wall_check_top.has_overlapping_bodies()
-	var ledge_clear = !ledge_check.has_overlapping_bodies()
-	return wall_side_touched and ledge_clear
-
-func is_pushing_against_wall(normal: Vector2) -> bool:
-	return (normal == Vector2(1.0, 0.0) and Input.is_action_pressed("Left")) or \
-		   (normal == Vector2(-1.0, 0.0) and Input.is_action_pressed("Right"))
-		
-func play_anim(name: String) -> void:
-	if $AnimatedSprite2D.animation != name:
-		$AnimatedSprite2D.play(name)
+var WallJumped = false
 
 func _physics_process(delta):
 	
@@ -43,19 +19,19 @@ func _physics_process(delta):
 	
 	var direction = Input.get_axis("Left", "Right")
 	
-	if direction and !CantWalk: 
+	if direction and !WallJumped: 
 		
 		velocity.x = direction * SPEED
 		
 		if is_on_floor():
-			play_anim("Run")
+			$AnimatedSprite2D.play("Run")
 		
-	elif !CantWalk: 
+	elif !WallJumped: 
 		
 		velocity.x = 0
 		
 		if is_on_floor():
-			play_anim("Idle")
+			$AnimatedSprite2D.play("Idle")
 	
 	#Rotate
 	
@@ -66,7 +42,7 @@ func _physics_process(delta):
 	
 	#Gravity
 	
-	if !is_on_floor() and !WallCling and !HangingFromWall:
+	if !is_on_floor() and !WallCling:
 		
 		velocity.y += GRAVITY * delta
 	
@@ -76,60 +52,35 @@ func _physics_process(delta):
 		
 		velocity.y -= JUMP_FORCE
 		
-		play_anim("Jump")
-	
-	#LedgeGrab
-	
-	if !is_on_floor() and velocity.y > 0 and is_touching_ledge_side() and !LedgeGrabRest:
-		LedgeGrabRest = true
-		CantWalk = true
-		velocity.y = 0
-		HangingFromWall = true
-	
-	if LedgeGrabRest and !is_touching_ledge_side() or LedgeGrabRest and is_on_floor():
-		LedgeGrabRest = false
-	
-	if HangingFromWall:
-		var wallCollisionSide = get_wall_normal()
-		if Input.is_action_pressed("Down") or wallCollisionSide == Vector2(1.0,0.0) and Input.is_action_pressed("Right") or wallCollisionSide == Vector2(-1.0,0.0) and Input.is_action_pressed("Left"):
-			CantWalk = false
-			HangingFromWall = false
-		
-		if Input.is_action_pressed("Up") or Input.is_action_just_pressed("Jump"):
-			CantWalk = false
-			HangingFromWall = false
-			velocity.y -= 300
+		$AnimatedSprite2D.play("Jump")
 	
 	#WallJump
 	
-	if !is_on_floor() and !WallClingRest and velocity.y > 0 and is_touching_wall_full_side():
-		var wallCollisionSide = get_wall_normal()
-		if is_pushing_against_wall(wallCollisionSide):
-			WallCling = true
-			WallClingRest = true
-			velocity.y = 0
-			await get_tree().create_timer(WALL_CLING_TIME).timeout
-			WallCling = false
+	if !is_on_floor() and !WallClingRest and velocity.y > 0:
+		if $RayCastLeftBottom.is_colliding() and $RayCastLeftTop.is_colliding() or $RayCastRightBottom.is_colliding() and $RayCastRightTop:
+			if $RayCastLeftBottom.get_collider() == $RayCastLeftTop.get_collider() or $RayCastRightBottom.get_collider() == $RayCastLeftTop.get_collider(): 
+				var wallCollisionSide = get_wall_normal()
+				if wallCollisionSide == Vector2(1.0,0.0) and Input.is_action_pressed("Left") or wallCollisionSide == Vector2(-1.0,0.0) and Input.is_action_pressed("Right"):
+					WallCling = true
+					WallClingRest = true
+					velocity.y = 0
+					await get_tree().create_timer(WALL_CLING_TIME).timeout
+					WallCling = false
 			
 	
-	if WallClingRest:
-		if is_on_floor():
-			WallClingRest = false
+	if is_on_floor() and WallClingRest:
+		WallClingRest = false
 		
-		if !is_on_wall():
-			WallCling = false
-	
-	if WallCling:
-		var wallCollisionSide = get_wall_normal()
-		if  !is_pushing_against_wall(wallCollisionSide):
-			WallCling = false
+	if !is_on_wall() and WallClingRest:
+		WallCling = false
 		
-		if Input.is_action_just_pressed("Jump"):
-			WallCling = false
-			CantWalk = true
-			velocity.y -= WALL_JUMP_FORCE
-			velocity.x = get_wall_normal().x * WALL_PUSH_FORCE
-			await get_tree().create_timer(WALL_JUMP_GRACE).timeout
-			CantWalk = false
+	if WallCling and Input.is_action_just_pressed("Jump"):
+		WallCling = false
+		WallJumped = true
+		velocity.y -= WALL_JUMP_FORCE
+		velocity.x = get_wall_normal().x * WALL_PUSH_FORCE
+		await get_tree().create_timer(WALL_JUMP_GRACE).timeout
+		WallJumped = false
 		
 	move_and_slide()
+	
